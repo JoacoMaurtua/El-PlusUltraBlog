e66e7e18ff0acba54535153a822aa3602c9cc612
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-09 13:45:12 -0500

    estilos globales

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/tailwind.css"; //Esta cabeza global siempre debe estar en el app.js
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
