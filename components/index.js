3ddbf64a744550a4f42556b6f90edf35a3096530
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-09 14:33:17 -0500

    estructura componente Header.js

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { default as PostCard } from './PostCard';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
+export { default as Header } from './Header';
 
 
 //Asi se puede importar facilmente cualquier componente o grupo 
