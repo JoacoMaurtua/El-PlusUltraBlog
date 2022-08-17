b03e1897c1e0785508c520bc413bf9d4c43c4f36
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-17 14:45:18 -0500

    estructura del formulario de comentarios terminada

--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react';
 
-const CommendsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  //Uso de useRef: No necesitamos mantener estos valores en el estado porque simplemente
+  // queremos leer el valor del campo de entrada y luego enviarlo a Graphql CMS
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  const handleCommentSubmit =()=>{
+
+  }
+
   return (
-    <div>CommendsForm</div>
-  )
-}
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments Form</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea 
+          ref={commentEl} 
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="p-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="p-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && <p className="text-xs text-red-500">All fields are required!</p>}
+      <div className="mt-8">
+        <button 
+          type="button" 
+          onClick={handleCommentSubmit}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+          >
+          Post Comment
+        </button>
+        {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment Submitted!</span>}
+      </div>
+    </div>
+  );
+};
 
-export default CommendsForm
+export default CommentsForm;
