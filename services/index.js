6a16d68a6bc0ee151d3ff959ef3a0d571eab8fc7
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-20 18:36:35 -0500

    controlador para recibir la lista de comentarios

--- services/index.js
+++ services/index.js
@@ -152,3 +152,19 @@ export const submitComment = async (obj) =>{
 
   return result.json();
 } 
+
+//Controlador para traer la lista de comentarios
+export const getComments = async (slug) =>{
+  const query = gql`
+    query GetComments($slug: String!){
+      comments(where: { post: {slug: $slug}}){
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
+}
