7d8231cb4d890307e33226cda7f74e2b61294781
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-21 22:47:33 -0500

    avance de FeaturedPosts

--- sections/FeaturedPosts.js
+++ sections/FeaturedPosts.js
@@ -1,16 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
-import "react-multi-carousel/lib/styles.css";
+import 'react-multi-carousel/lib/styles.css';
 
-import {FeaturedPostCard} from '../components';
+import { FeaturedPostCard } from '../components';
+import { getFeaturedPosts } from '../services';
 //Aqui debo obtener los posts de graphql y renderizarlo en un carousel
 
-
-
 const FeaturedPosts = () => {
-  return (
-    <div>FeaturedPosts</div>
-  )
-}
+  const [featuredPosts, setFeaturedPosts] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-export default FeaturedPosts
+  useEffect(() => {
+    getFeaturedPosts().then((result) => {
+      setFeaturedPosts(result);
+      setDataLoaded(true);
+    });
+  }, []);
+
+  //este carousel se implementa dandole estilos primero a sus flechas
+  const customLeftArrow = (
+    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+      </svg>
+    </div>
+  );
+
+  const customRightArrow = (
+    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+      </svg>
+    </div>
+  );
+
+  return (
+    <div>
+
+    </div>
+  ) 
+};
+
+export default FeaturedPosts;
